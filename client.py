ab4c05671d99b4b8d7156593579f5a92beaa14ab
egorl <egor.liap@icloud.com>
2024-07-12 19:02:25 +0300

    added data validation

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 from aiohttp import ClientSession
+import asyncio
 from dotenv import load_dotenv
 from os import getenv
+from validation import *
 load_dotenv()
 TOKEN = getenv("TOKEN")
+
+test_url = "https://games-test.datsteam.dev/"
+participate_endpoint = "play/zombidef/participate"
+commands_endpoint = "play/zombidef/command"
+units_endpoint = "play/zombidef/units"
+world_endpoint = "play/zombidef/world"
+rounds_endpoint = "rounds/zombidef"
 
 class Labaratory:
     """Класс, описывающий интерфейс взаимодействия с нашей лабараторией (на сервере организатора).
     Содержит методы, реализующие данное апи"""
-    def get_info_1(self):
-        pass
-    def get_info_2(self):
-        pass
-    def post_info_1(self):
-        pass
+    
+    def __init__(self,session:ClientSession):
+        self.session = session
+        
+    
+
--- validation.py
+++ validation.py
@@ -1,13 +1,100 @@
 """Ответы от сервера приходят в json, для валидации используем датаклассы pydantic
     Экземпляры этих датаклассов идут в логику и там обрабатываются"""
+import datetime
 import pydantic
 
-class Unit1(pydantic.BaseModel):
-    pos1:type
-    pos2:type
-    pos3:type
+class BaseRequest(pydantic.BaseModel):
+    api_key:str = pydantic.Field(alias="X-Auth-Token")
+     
+class BaseResponse(pydantic.BaseModel):
+    errors:list[str|None]
     
-class Unit2(pydantic.BaseModel):
-    pos1:type
-    pos2:type
-    pos3:type
+class Coords(pydantic.BaseModel):
+    x:int
+    y:int
+
+class AttackCommand(pydantic.BaseModel):
+    block_id:str = pydantic.Field(alias='blockId')
+    target:Coords
+
+class AcceptedCommands(pydantic.BaseModel):
+    attack:list[AttackCommand]
+    build:list[Coords]
+    move_base:Coords
+    
+class Commands(BaseResponse):
+    accepted_commands:AcceptedCommands = pydantic.Field(alias='acceptedCommands')
+
+class Participate(pydantic.BaseModel):
+    start_in:int = pydantic.Field(alias="startsInSec")
+
+class Block(pydantic.BaseModel):
+    attack:int
+    health:int
+    id:str
+    range:int
+    is_head:bool = pydantic.Field(alias="isHead")
+    last_attack:Coords = pydantic.Field(alias="lastAttack")
+    name:str
+    x:int
+    y:int
+    
+class EnemyBlock(pydantic.BaseModel):
+    attack:int
+    health:int
+    is_head:bool = pydantic.Field(alias="isHead")
+    last_attack:Coords = pydantic.Field(alias="lastAttack")
+    name:str
+    x:int
+    y:int
+    
+class Player(pydantic.BaseModel):
+    enemy_block_kills:int = pydantic.Field(alias="enemyBlockKills")
+    game_ended_at:datetime = pydantic.Field(alias="gameEndedAt")
+    gold:int
+    name:str
+    points:int
+    zombie_kills:int = pydantic.Field(alias="zombieKills")
+        
+class Zombie(pydantic.BaseModel):
+    attack:int
+    direction:str
+    health:int
+    id:str
+    speed:int
+    type:str
+    wait_turns:int = pydantic.Field(alias='waitTurns')
+    x:int
+    y:int
+
+class Zpot(pydantic.BaseModel):
+    x:int
+    y:int
+    type:str
+
+class UnitsChanging(pydantic.BaseModel):
+    base:list[Block]
+    enemy_blocks:list[EnemyBlock] = pydantic.Field(alias='enemyBlocks')
+    player:Player
+    realm_name:str = pydantic.Field(alias="realmName")
+    turn:int
+    turn_ends_in_ms:int = pydantic.Field(alias="turnEndsInMs")    
+    zombies:list[Zombie]
+
+class UnitsNotChanging(pydantic.BaseModel):
+    realm_name:str = pydantic.Field(alias="realmName")
+    zpots:list[Zpot]
+    
+class Round(pydantic.BaseModel):
+    duration:int
+    end_at:datetime = pydantic.Field(alias="endAt")
+    name:str
+    repeat:int
+    start_at:datetime = pydantic.Field(alias="startAt")
+    status:str
+    
+class GameRounds(pydantic.BaseModel):
+    game_name:str = pydantic.Field(alias="gameName")
+    now:datetime
+    rounds:list[Round]
+    
