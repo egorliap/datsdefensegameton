7a94ac790eff80a3e5db9ba2518b0c68941960ae
egorl <egor.liap@icloud.com>
2024-07-12 19:53:06 +0300

    added data validation errors

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 from aiohttp import ClientSession
+import requests
 import asyncio
+from validation import *
 from dotenv import load_dotenv
 from os import getenv
-from validation import *
+
 load_dotenv()
 TOKEN = getenv("TOKEN")
 
-test_url = "https://games-test.datsteam.dev/"
+url = "https://games-test.datsteam.dev/"
+
 participate_endpoint = "play/zombidef/participate"
 commands_endpoint = "play/zombidef/command"
 units_endpoint = "play/zombidef/units"
@@ -17,8 +20,32 @@ class Labaratory:
     """Класс, описывающий интерфейс взаимодействия с нашей лабараторией (на сервере организатора).
     Содержит методы, реализующие данное апи"""
     
-    def __init__(self,session:ClientSession):
-        self.session = session
+    def __init__(self,session:requests.Session):
+        self.session:requests.Session = session
+        self.session.headers.update({"X-Auth-Token":TOKEN})
+    def commands(self,payload):
+        pass
+    def participate(self):
+        res = self.session.put(url=url+participate_endpoint)
+        if(res.status_code != 200):
+            return Error.model_validate_json(res.content)
+        return res.content
+    def rounds(self):
+        pass
+    
+    def units(self):
+        res = self.session.get(url=url+units_endpoint)
+        if(res.status_code != 200):
+            return Error.model_validate_json(res.content)
+        return res.content
+
         
     
+    def world(self):
+        pass
+        
+    
+session = requests.Session()
+l = Labaratory(session)
 
+print(l.participate())
--- validation.py
+++ validation.py
@@ -1,20 +1,17 @@
 """Ответы от сервера приходят в json, для валидации используем датаклассы pydantic
     Экземпляры этих датаклассов идут в логику и там обрабатываются"""
-import datetime
 import pydantic
 
 class BaseRequest(pydantic.BaseModel):
-    api_key:str = pydantic.Field(alias="X-Auth-Token")
+    api_key:str = pydantic.Field(validation_alias="X-Auth-Token")
      
-class BaseResponse(pydantic.BaseModel):
-    errors:list[str|None]
     
 class Coords(pydantic.BaseModel):
     x:int
     y:int
 
 class AttackCommand(pydantic.BaseModel):
-    block_id:str = pydantic.Field(alias='blockId')
+    block_id:str = pydantic.Field(validation_alias='blockId')
     target:Coords
 
 class AcceptedCommands(pydantic.BaseModel):
@@ -22,39 +19,40 @@ class AcceptedCommands(pydantic.BaseModel):
     build:list[Coords]
     move_base:Coords
     
-class Commands(BaseResponse):
-    accepted_commands:AcceptedCommands = pydantic.Field(alias='acceptedCommands')
+class Commands(pydantic.BaseModel):
+    accepted_commands:AcceptedCommands = pydantic.Field(validation_alias='acceptedCommands')
+    errors:list[str|None]
 
 class Participate(pydantic.BaseModel):
-    start_in:int = pydantic.Field(alias="startsInSec")
+    start_in:int = pydantic.Field(validation_alias="startsInSec")
 
 class Block(pydantic.BaseModel):
     attack:int
     health:int
     id:str
     range:int
-    is_head:bool = pydantic.Field(alias="isHead")
-    last_attack:Coords = pydantic.Field(alias="lastAttack")
-    name:str
+    is_head:bool|None = pydantic.Field(validation_alias="isHead")
+    last_attack:Coords|None = pydantic.Field(validation_alias="lastAttack")
+    name:str|None
     x:int
     y:int
     
 class EnemyBlock(pydantic.BaseModel):
     attack:int
     health:int
-    is_head:bool = pydantic.Field(alias="isHead")
-    last_attack:Coords = pydantic.Field(alias="lastAttack")
+    is_head:bool = pydantic.Field(validation_alias="isHead")
+    last_attack:Coords = pydantic.Field(validation_alias="lastAttack")
     name:str
     x:int
     y:int
     
 class Player(pydantic.BaseModel):
-    enemy_block_kills:int = pydantic.Field(alias="enemyBlockKills")
-    game_ended_at:datetime = pydantic.Field(alias="gameEndedAt")
+    enemy_block_kills:int = pydantic.Field(validation_alias="enemyBlockKills")
+    game_ended_at:str|None = pydantic.Field(validation_alias="gameEndedAt")
     gold:int
     name:str
     points:int
-    zombie_kills:int = pydantic.Field(alias="zombieKills")
+    zombie_kills:int = pydantic.Field(validation_alias="zombieKills")
         
 class Zombie(pydantic.BaseModel):
     attack:int
@@ -63,7 +61,7 @@ class Zombie(pydantic.BaseModel):
     id:str
     speed:int
     type:str
-    wait_turns:int = pydantic.Field(alias='waitTurns')
+    wait_turns:int = pydantic.Field(validation_alias='waitTurns')
     x:int
     y:int
 
@@ -74,27 +72,39 @@ class Zpot(pydantic.BaseModel):
 
 class UnitsChanging(pydantic.BaseModel):
     base:list[Block]
-    enemy_blocks:list[EnemyBlock] = pydantic.Field(alias='enemyBlocks')
+    enemy_blocks:list[EnemyBlock] = pydantic.Field(validation_alias='enemyBlocks')
     player:Player
-    realm_name:str = pydantic.Field(alias="realmName")
+    realm_name:str = pydantic.Field(validation_alias="realmName")
     turn:int
-    turn_ends_in_ms:int = pydantic.Field(alias="turnEndsInMs")    
+    turn_ends_in_ms:int = pydantic.Field(validation_alias="turnEndsInMs")    
     zombies:list[Zombie]
 
 class UnitsNotChanging(pydantic.BaseModel):
-    realm_name:str = pydantic.Field(alias="realmName")
+    realm_name:str = pydantic.Field(validation_alias="realmName")
     zpots:list[Zpot]
     
 class Round(pydantic.BaseModel):
     duration:int
-    end_at:datetime = pydantic.Field(alias="endAt")
+    end_at:str = pydantic.Field(validation_alias="endAt")
     name:str
     repeat:int
-    start_at:datetime = pydantic.Field(alias="startAt")
+    start_at:str = pydantic.Field(validation_alias="startAt")
     status:str
     
 class GameRounds(pydantic.BaseModel):
-    game_name:str = pydantic.Field(alias="gameName")
-    now:datetime
+    game_name:str = pydantic.Field(validation_alias="gameName")
+    now:str
     rounds:list[Round]
     
+    
+class BaseRequest(pydantic.BaseModel):
+    api_key:str = pydantic.Field(validation_alias="X-Auth-Token")
+
+class CommandRequest(pydantic.BaseModel):
+    attack:list[AttackCommand]
+    build:list[Coords]
+    move_base:Coords = pydantic.Field(validation_alias="moveBase")
+    
+class Error(pydantic.BaseModel):
+    err_code:int = pydantic.Field(validation_alias='errCode')
+    error:str
